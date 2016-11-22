d76446ae0156e7c12488636cfa7950474ec9715f
dancemarket <dhgofls@nate.com>
2016-11-22 15:35:17 +0900

    피보나치 종료 해보삼

--- src/fibonacci/version2/CacheFibonacci.java
+++ src/fibonacci/version2/CacheFibonacci.java
@@ -1,38 +1,47 @@
 package fibonacci.version2;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class CacheFibonacci {
 
-    public static void main(String[] args) {
-        fiboWork();
+    static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    static Scanner scan = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String cuti = "xx";
+        while(cuti.equals("xx")){
+            fiboWork();
+            System.out.println("중단하고 싶으면 end를 계속하고 싶으면 아무키입력 후 엔터");
+            cuti = scan.next();
+            if(cuti.equals("end")){
+                System.out.println("피보나치 종료");
+            }else{
+                cuti = "xx";
+            }
+        }
     }
     public static void fiboWork(){
-        Scanner scan = new Scanner(System.in);
         System.out.println("피보나치 수열을 구하고 싶은 숫자를 입력하시오");
         int fiboNum = scan.nextInt();
-        System.out.println(fibo(fiboNum));
+        System.out.println("피보나치 수: " + fibo(fiboNum));
     }
     public static int fibo(int n) {
         int[] arry = new int[n];
-        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
-        if (m.containsKey(n-1)) {
-            System.out.println("캐시에서 가져옴");
-            return m.get(n-1);
-        }else{
+        if (map.containsKey((n-1))) {
+            //System.out.println("캐시에서 가져옴");
+            return map.get((n-1));
+        }else {
             for (int i = 0; i < n; i++) {
-                if (i<2){
+                if (i < 2) {
                     arry[i] = 1;
-                }else{
-                    arry[i] = arry[i-2] + arry[i-1];
+                } else {
+                    arry[i] = arry[i - 2] + arry[i - 1];
                 }
-                System.out.println("처음가져옴");
+                //System.out.println("처음가져옴");
+                map.put(i, (arry[i]));
             }
-            m.put(n-1,arry[n-1]);
-            return m.get(n-1);
+            return map.get((n - 1));
         }
     }
 }
