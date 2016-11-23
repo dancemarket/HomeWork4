6cef1c44d8690be7f09bff5cdc1684db09f78871
dancemarket <dhgofls@nate.com>
2016-11-23 15:39:46 +0900

    괄호 검사 완료

--- src/homework1117/HomeWork1177.java
+++ src/homework1117/HomeWork1177.java
@@ -5,20 +5,45 @@ package homework1117;
  */
 public class HomeWork1177 {
 
-	//
-	// 열기닫기 검사하기 툴
-	// {} => 성공
-	// {}} => 실패
-	// {[]} => 성공
-	// {[}] => 실패
-	// stack을 이용하면 돼
 	public static void main(String[] args) {
-		System.out.println(check("df{{,,...}}}d"));
+		System.out.println(check("uf{}u[]iyg[]iug"));
 	}
 
-	public static String check(String data) {
-		String iget = data;
-		iget = iget.replaceAll("[^{,^},^[,^]]", "");
-		return iget;
-	}
+	public static boolean check(String data) {
+		String nget = "";
+        int a = 0, b = 0, A = 0, B = 0;
+        //필요한 괄호 정렬
+		for (int i = 0; i < data.length(); i++) {
+			if (data.charAt(i) == '{' || data.charAt(i) == '}' || data.charAt(i) == '[' || data.charAt(i) == ']'){
+				nget = nget + data.charAt(i);
+                switch (data.charAt(i)){
+                    case '{' :
+                        a++;
+                        break;
+                    case '[' :
+                        b++;
+                        break;
+                    case '}' :
+                        A++;
+                        break;
+                    case ']' :
+                        B++;
+                        break;
+                    default:
+                        break;
+                }
+			}
+		}
+
+		if ((a == A && b == B)&&(nget.charAt(0)=='{'||nget.charAt(0)=='[')&&(nget.charAt(nget.length()-1)=='}'||nget.charAt(nget.length()-1)==']')){
+            for(int i = 1; i < nget.length();i++){
+                if((nget.charAt(i-1)=='['&&nget.charAt(i)=='}')||(nget.charAt(i-1)=='{'&&nget.charAt(i)==']')) {
+                    break;
+                }else if(i == nget.length()-1){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
