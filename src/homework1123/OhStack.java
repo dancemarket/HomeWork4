e4d35423a09c2795e5a49468cf6a8475aff1790e
THEOH\dhgof_000 <speedy.oh5@gamil.com>
2016-11-23 23:02:36 +0900

    새로운 숙제 해보자

---
+++ src/homework1123/OhStack.java
@@ -0,0 +1,28 @@
+package homework1123;
+
+/**
+ * Created by dhgof_000 on 2016-11-23.
+ */
+public class OhStack {
+
+    // boolean	empty() : Tests if this stack is empty.
+    public boolean empty() {
+        return false;
+    }
+
+    //  E	push(E item) : Pushes an item onto the top of this stack.
+    public char push(char c) {
+        return c;
+    }
+
+    //  E	pop() : Removes the object at the top of this stack and returns that object as the value of this function.
+    public char pop() {
+        return 0;
+    }
+
+    //  E	peek() : Looks at the object at the top of this stack without removing it from the stack.
+    public char peek() {
+        return 0;
+    }
+
+}
---
+++ src/homework1123/OhStackTest.java
@@ -0,0 +1,30 @@
+package homework1123;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dhgof_000 on 2016-11-23.
+ */
+public class OhStackTest {
+
+    @Test
+    public void testEmpty() {
+        OhStack stack = new OhStack();
+
+        assertTrue(stack.empty());
+
+        stack.push('(');
+        assertFalse(stack.empty());
+
+        assertTrue(stack.peek() == '(');
+        assertTrue(stack.pop() == '(');
+
+        assertTrue(stack.empty());
+    }
+
+
+
+
+}
