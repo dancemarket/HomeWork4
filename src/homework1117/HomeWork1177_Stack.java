5260bf1667e068178af75ddb5c87f64dfb92608d
dancemarket <dhgofls@nate.com>
2016-12-01 09:43:41 +0900

    ohstack 객체 생성함

--- src/homework1117/HomeWork1177_Stack.java
+++ src/homework1117/HomeWork1177_Stack.java
@@ -12,42 +12,43 @@ public class HomeWork1177_Stack {
     }
 
     public static boolean check(String data) {
+        OhStack ohStack = new OhStack();
         /*//OhStack에 잘 들어가는지 확인 = 잘 들어감
-        OhStack.push((Character.toString(data.charAt(0))));
-        System.out.println(OhStack.peek());
+        ohStack.push((Character.toString(data.charAt(0))));
+        System.out.println(ohStack.peek());
         */
         for (int i = 0; i < data.length(); i++) {
             if ((Character.toString(data.charAt(i))).equals("(")
                     || (Character.toString(data.charAt(i))).equals("{")
                     || (Character.toString(data.charAt(i))).equals("[")){
-                OhStack.push((Character.toString(data.charAt(i))));
+                ohStack.push((Character.toString(data.charAt(i))));
                 //if 잘 돌아가는지 확인
                 //System.out.println(OhStack.peek());
             }else if ((Character.toString(data.charAt(i))).equals(")")){
-                if (((OhStack.peek()).equals("("))){
+                if (((ohStack.peek()).equals("("))){
                     //if 잘 돌아가는지 확인
                     //System.out.println(OhStack.peek());
-                    OhStack.pop();
+                    ohStack.pop();
                 }else{
                     return false;
                 }
             }else {
                 if ((Character.toString(data.charAt(i))).equals("}")) {
-                    if (((OhStack.peek()).equals("{"))) {
-                        OhStack.pop();
+                    if (((ohStack.peek()).equals("{"))) {
+                        ohStack.pop();
                     } else {
                         return false;
                     }
                 } else {
-                    if (((OhStack.peek()).equals("["))) {
-                        OhStack.pop();
+                    if (((ohStack.peek()).equals("["))) {
+                        ohStack.pop();
                     } else {
                         return false;
                     }
                 }
             }
         }
-        if (OhStack.empty()){
+        if (ohStack.empty()){
             return true;
         }
         return false;
