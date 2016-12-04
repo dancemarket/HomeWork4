5486d9ff999b532030d3ddd91a71ae931f31972f
THEOH\dhgof_000 <speedy.oh5@gamil.com>
2016-12-04 17:04:18 +0900

    링크드 리스트 구현

---
+++ src/homework1201_LinkedList/OhLinkedList_mainCheck.java
@@ -0,0 +1,16 @@
+package homework1201_LinkedList;
+
+/**
+ * Created by dhgof_000 on 2016-12-04.
+ */
+public class OhLinkedList_mainCheck {
+    public static void main(String[] args) {
+        OhLinkedList linkedList = new OhLinkedList();
+        linkedList.addHead(3);
+        linkedList.addTail(6);
+        linkedList.add(1,4);
+        linkedList.add(2,8);
+        linkedList.print();
+    }
+
+}
