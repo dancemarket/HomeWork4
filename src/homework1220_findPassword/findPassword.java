e050f4d857b233b59af06341738c2ca137220383
THEOH\dhgof_000 <speedy.oh5@gamil.com>
2016-12-21 01:07:27 +0900

    ddd

--- src/homework1220_findPassword/findPassword.java
+++ src/homework1220_findPassword/findPassword.java
@@ -20,6 +20,28 @@ public class findPassword {
     }
 
     public void passwordMake(int L, int C){
-        String[] lChar = new String[L];
+        String[] tempChar = new String[L];
+        String[] getChar = new String[C];
+        ArrayList<String> passwordList = new ArrayList<>();
+        //예상되는 문자 입력 받기
+        for (int i = 0; i < C; i++) {
+            getChar[i] = scanCh.next();
+        }
+        //문자안에 자음2개 이상 모음 1개 이상 있는지 검증 -> 이건 tempChar에 넣어놓고 비교 해보자
+        //비교하면서 카운트 하고, 카운트 한것이 충분하다면 ArrayList에 저장해주기
+
+        //문자 하나씩 알파벳 순서 비교하면서 tempChar에 넣어주기
+        for (int i = 0; i < C; i++) {
+            tempChar[0] = getChar[i];
+            for (int j = 1; j < L; j++) {
+                tempChar[j] = getChar[i];
+                for (int k = 1; k < ; k++) {
+
+                }
+            }
+
+        }
+
+
     }
 }
