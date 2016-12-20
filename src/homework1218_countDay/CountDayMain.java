5ecf82bdcd480498f85b1f81febeeb08c340fba9
dancemarket <dhgofls@nate.com>
2016-12-20 10:12:34 +0900

    날자 계산 고급형 완료

--- src/homework1218_countDay/CountDayMain.java
+++ src/homework1218_countDay/CountDayMain.java
@@ -7,9 +7,21 @@ public class CountDayMain {
 
     public static void main(String[] args) {
         CountDayWork cdw = new CountDayWork();
+        CountDayWorkHighGrade cdwhg = new CountDayWorkHighGrade();
 
-        int day = cdw.calenderCount(2016, 12,5, 2015, 11, 31);
-        System.out.println(day);
+        int day = cdw.calenderCount("2016-12-5", "2015-11-31");
+        System.out.println(day + "일전");
+
+        String dayCount = cdwhg.calenderCount("2016-11-16", "2016-10-16");
+        System.out.println(dayCount);
+
+        //String to Integer 화
+        /*
+        String wholeDate = "2016-12-5";
+        String[] date = wholeDate.split("-");
+        int year = Integer.parseInt(date[0]);
+        System.out.println(year);
+        */
     }
 
 
--- src/homework1218_countDay/CountDayWorkHighGrade.java
+++ src/homework1218_countDay/CountDayWorkHighGrade.java
@@ -1,16 +1,12 @@
 package homework1218_countDay;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
 /*
  * Created by dhgof_000 on 2016-12-18.
  */
 
-public class CountDayWork {
+public class CountDayWorkHighGrade {
 
-    public int calenderCount(String present, String past){
+    public String calenderCount(String present, String past){
         //넘어온 일자를 숫자로 변경들 해주시고
         String[] preDate = present.split("-");
         int preYear = Integer.parseInt(preDate[0]);
@@ -24,7 +20,15 @@ public class CountDayWork {
         //함수 이용해서 변수에 총 날자들 계산해준걸 담아주고
         int presentDate = calculateDay(preYear, preMon, preDay);
         int pastDate = calculateDay(pasYear, pasMon, pasDay);
-        return presentDate - pastDate;
+        int countDate = presentDate - pastDate;
+        if (countDate == 0){
+            return "오늘";
+        }else if(countDate == 1){
+            return "어제";
+        }else if(countDate == 30){
+            return "한달전";
+        }
+        return countDate+"일전";
     }
     public int calculateDay(int year, int mon, int day){
         int countDay = 0;
