2003a1ec27c01cbed01bb5b2dfb82a2db1a0b20b
THEOH\dhgof_000 <speedy.oh5@gamil.com>
2016-12-21 10:25:28 +0900

    이번 문제는 어렵구나

--- src/homework1220_findPassword/findMain.java
+++ src/homework1220_findPassword/findMain.java
@@ -1,5 +1,8 @@
 package homework1220_findPassword;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by 205-121 on 2016-12-20.
  */
@@ -46,13 +49,19 @@ package homework1220_findPassword;
 
 public class findMain {
     public static void main(String[] args) {
-        char ch = 'a';
-        char ch2 = 'b';
-
-        if (ch > ch2){
-            System.out.println(ch);
-        }else{
-            System.out.println("앞에 문자가 더 작습니다");
+        //문자를 제대로 입력 받는가
+        Scanner scan = new Scanner(System.in);
+        String[] getChar = new String[6];
+        //예상되는 문자 입력 받기
+        for (int i = 0; i < 6; i++) {
+            getChar[i] = scan.next();
         }
+        Arrays.sort(getChar);
+        System.out.println(Arrays.toString(getChar));
+    /*
+        findPassword fp = new findPassword();
+
+        System.out.println(fp.passwordFind());
+*/
     }
 }
