65e335807b199ca46f98232f07977a65ffad3135
THEOH\dhgof_000 <speedy.oh5@gamil.com>
2016-12-30 01:57:30 +0900

    배열복사하고 정렬하기 정리함

--- src/homework1226_sumArray/SumArray.java
+++ src/homework1226_sumArray/SumArray.java
@@ -9,47 +9,29 @@ import java.util.List;
  * Created by dhgof_000 on 2016-12-26.
  * 3개의 배열을 오름차순으로 “정렬된 하나의 배열”로 합치시오.
 
- Input
- arr1 = { 3, 6, 4, 7, 1 }
- arr2 = { 10, 5, 2, 43, 33, 56 }
- arr3 = { 8, 78, 44, 52, 9 }
-
- Output
- { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 33, 43, 44, 52, 56, 78 }
-
- sorting 알고리즘은 구현하지 않음. 본인이 알고있는 라이브러리 사용
+ * 정렬 알고리즘 구현해서 출력하도록.
  */
 public class SumArray {
     public static void main(String[] args) {
         int[] arr1 = {3,6,4,7,1};
         int[] arr2 = {10,5,2,43,33,56};
         int[] arr3 = {8,78,44,52,9};
-        int num1 = arr1.length;
-        int num2 = arr2.length;
-        int num3 = arr3.length;
-        List<Integer> collectArray = new ArrayList<>();
-        for (int i = 0; i < num1; i++) {
-            collectArray.add(arr1[i]);
-        }
-        for (int i = 0; i < num2; i++) {
-            collectArray.add(arr2[i]);
-        }
-        for (int i = 0; i < num3; i++) {
-            collectArray.add(arr3[i]);
-        }
 
+        int[] gettingArray = new int[arr1.length + arr2.length + arr3.length];
 
-        for (int i = 0; i < collectArray.size()-1; i++) {
-            for (int j = 1; j < collectArray.size(); j++) {
-                if(collectArray.get(i) > collectArray.get(j)){
-                    int temp = collectArray.get(j);
-                    Integer s = collectArray.get(i);
-                    s = temp;
+        System.arraycopy(arr1, 0, gettingArray, 0, arr1.length);
+        System.arraycopy(arr2, 0, gettingArray, arr1.length, arr2.length);
+        System.arraycopy(arr3, 0, gettingArray, arr2.length + arr3.length, arr3.length);
+
+        for(int i=1; i < gettingArray.length ; i++){
+            for(int j=0; j < gettingArray.length-i ; j++){
+                if(gettingArray[j] > gettingArray[j+1]){
+                    int temp=gettingArray[j];
+                    gettingArray[j]=gettingArray[j+1];
+                    gettingArray[j+1]=temp;
                 }
             }
         }
-
-        System.out.println(collectArray);
-
+        System.out.println(Arrays.toString(gettingArray));
     }
 }
