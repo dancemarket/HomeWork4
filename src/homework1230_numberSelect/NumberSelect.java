f316664ceb447931bb27cebff2b19c73e539f128
THEOH\dhgof_000 <speedy.oh5@gamil.com>
2016-12-30 02:01:48 +0900

    로또문제인가

---
+++ src/homework1230_numberSelect/NumberSelect.java
@@ -0,0 +1,13 @@
+package homework1230_numberSelect;
+
+/**
+ * Created by dhgof_000 on 2016-12-30.
+ */
+/*
+총 수가 15개 인 로또 6개 고르기…
+        조건.. 중복은 안되고,
+        모든 수의 합이 3, 5의 배수면 안돼
+*/
+public class NumberSelect {
+
+}
