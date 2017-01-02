10a013e594e3485316d695d7dbbe6af7c8e81e30
dancemarket <dhgofls@nate.com>
2017-01-02 14:47:36 +0900

    중앙값 구해서 총합을 구하기 부제 : 기상 온도 파악

---
+++ src/homework170102_medianValue/MedianValueMain.java
@@ -0,0 +1,32 @@
+package homework170102_medianValue;
+
+import java.util.Scanner;
+
+/**
+ * Created by 205-121 on 2017-01-02.
+ */
+public class MedianValueMain {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int K = scan.nextInt();
+        int[] nArray = new int[N];
+        int[] kArray = new int[K];
+        int total = 0;
+        for (int i = 0; i < nArray.length; i++) {
+            if (i<K-1){
+                nArray[i] = scan.nextInt();
+            }else {
+                nArray[i] = scan.nextInt();
+                int count = 0;
+                for (int j = i; j > i-K; j--) {
+                    kArray[count] = nArray[j];
+                    count++;
+                }
+                total = total + kArray[1];
+            }
+        }
+        System.out.println(total);
+    }
+
+}
