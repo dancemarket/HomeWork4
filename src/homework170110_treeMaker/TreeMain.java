71e4965b7e2f6f6b2f867fc3a7dfac4638d5c18b
THEOH\dhgof_000 <speedy.oh5@gamil.com>
2017-01-14 18:29:40 +0900

    in-order pre-order post-order level-order 완벽 구현

--- src/homework170110_treeMaker/TreeMain.java
+++ src/homework170110_treeMaker/TreeMain.java
@@ -1,5 +1,7 @@
 package homework170110_treeMaker;
 
+import java.util.ArrayList;
+
 /**
  * Created by dhgof_000 on 2017-01-10.
  */
@@ -21,10 +23,18 @@ public class TreeMain {
 
 
 //        System.out.println(binaryTree.root.right.right.data);
+        System.out.print("In-Order 출력 : ");
         binaryTree.inOrder();
         System.out.println("");
+        System.out.print("Pre-Order 출력 : ");
         binaryTree.preOrder();
         System.out.println("");
+        System.out.print("Post-Order 출력 : ");
         binaryTree.postOrder();
+        System.out.println("");
+        //레벨 탐색형이라면 아래와 같이 출력되어야 함
+        //P F S B H R Y G T Z W
+        System.out.print("Level-Order 출력(index 0 부터 1 레벨): ");
+        binaryTree.levelOrder();
     }
 }
