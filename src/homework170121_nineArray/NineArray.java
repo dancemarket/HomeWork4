7e5b8188ab742ecede28051286ffbaecd38f3fcf
THEOH\dhgof_000 <speedy.oh5@gamil.com>
2017-01-21 19:18:47 +0900

    개의 배열에 값 넣기

---
+++ src/homework170121_nineArray/NineArray.java
@@ -0,0 +1,16 @@
+package homework170121_nineArray;
+
+import java.util.Arrays;
+
+/**
+ * Created by dhgof_000 on 2017-01-21.
+ */
+public class NineArray {
+    static Integer[] array = new Integer[9];
+    public static void main(String[] args) {
+        Stack1 stack1 = new Stack1();
+        stack1.push(1);
+        System.out.println(Arrays.toString(array));
+    }
+
+}
---
+++ src/homework170121_nineArray/Stack2.java
@@ -0,0 +1,32 @@
+package homework170121_nineArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static homework170121_nineArray.NineArray.array;
+
+/**
+ * Created by dhgof_000 on 2017-01-21.
+ */
+public class Stack2 {
+    int index;
+    List<Integer> list = new ArrayList<>();
+    public void push(int number){
+        if (index < 3){
+            list.add(number);
+            array[index+3] = list.get(index);
+            index++;
+        }else {
+            System.out.println("스택을 초과 하였습니다");
+        }
+    }
+    public void pop(){
+        if (index > -1){
+            list.set(index, null);
+            array[index+3] = list.get(index);
+            index--;
+        }else {
+            System.out.println("스택이 비어 있습니다");
+        }
+    }
+}
